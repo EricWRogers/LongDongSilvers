95dd4552ce5754d50bf5578f663759bc56f444da
dylan <ghilliard69420@gmail.com>
2026-06-26 22:42:08 -0500

    MONEY IN HUD

--- Assets/Scripts/HudManager.cs
+++ Assets/Scripts/HudManager.cs
@@ -4,9 +4,13 @@ using TMPro;
 public class HUDManager : MonoBehaviour
 {
     public TMP_Text clockText;
+    public TMP_Text moneyText;
 
     void Update()
     {
-        clockText.text = GameManager.Instance.GetFormattedTime();
+            clockText.text = GameManager.Instance.GetFormattedTime();
+
+            moneyText.text = $"${RestaurantMoney.Instance.Money}";
+
     }
 }
